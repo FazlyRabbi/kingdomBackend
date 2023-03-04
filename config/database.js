efcc5c7731f039953b85f577bb2808e345625426
FazlyRabbi <rabbisam74@gmail.com>
2023-03-04 22:01:04 +0600

    fix the problem

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5669),
+      host: env('DATABASE_HOST', 'containers-us-west-55.railway.app'),
+      port: env.int('DATABASE_PORT', 7428),
       database: env('DATABASE_NAME', 'railway'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'jAQgLUHd7xgK9CfTBJTm'),
