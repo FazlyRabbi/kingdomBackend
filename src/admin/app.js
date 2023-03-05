f370972096c61b60bdfd11068454bfdf2907e80e
FazlyRabbi <rabbisam74@gmail.com>
2023-03-05 14:36:19 +0600

    change the text

---
+++ src/admin/app.js
@@ -0,0 +1,16 @@
+
+
+export default {
+  config: {
+    locales: ["en"],
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "Welcome to kingdomofkush!",
+        "Auth.form.welcome.subtitle": "Log in to your Admin account",
+        "app.components.LeftMenu.navbrand.title": "Dashboard",
+      },
+    }
+ 
+  },
+  bootstrap() {},
+};
