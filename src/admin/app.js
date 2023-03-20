d3f7e3153c3346fe7a4fcb6d60e2559fc19a4282
FazlyRabbi <rabbisam74@gmail.com>
2023-03-20 20:47:57 +0600

    change text from welcome page

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,5 +1,3 @@
-
-
 export default {
   config: {
     locales: ["en"],
@@ -8,9 +6,23 @@ export default {
         "Auth.form.welcome.title": "Welcome to kingdomofkush!",
         "Auth.form.welcome.subtitle": "Log in to your Admin account",
         "app.components.LeftMenu.navbrand.title": "Dashboard",
+        "app.components.HomePage.welcome.again":
+          "WELCOME TO KINGDOM OF KUSH ADMIN DASHBOARD",
+        "app.components.HomePage.welcomeBlock.content.again": `TOGETHER WE PROSPER. THE FUTURE IS NOW
+        Kingdom Of Kush
+        Kingdom of Kush LLC
+        One World Trade Center Floor 85
+        New York, NY 10007
+        USA`,
+        "app.components.HomePage.button.blog": ` 󠀠`,
       },
-    }
- 
+    },
   },
+
+  // Disable video tutorials
+  tutorials: false,
+  // Disable notifications about new Strapi releases
+  notifications: { releases: false },
+
   bootstrap() {},
 };
