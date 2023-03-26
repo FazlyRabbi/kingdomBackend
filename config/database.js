389f45f10f93bb8a97b84588dff4a1b4586b0f2a
FazlyRabbi <rabbisam74@gmail.com>
2023-03-26 09:39:35 +0600

    add db config

--- config/database.js
+++ config/database.js
@@ -3,14 +3,16 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'containers-us-west-55.railway.app'),
-      port: env.int('DATABASE_PORT', 7428),
-      database: env('DATABASE_NAME', 'railway'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'jAQgLUHd7xgK9CfTBJTm'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST', '40.74.228.100'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'king_db'),
+      user: env('DATABASE_USERNAME', 'king_user'),
+      password: env('DATABASE_PASSWORD', 'King@2023'),
+      // ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
 
 
+
+// The postgresql connection: 40.74.228.100 port: 5432 , username: king_user  , pass: King@2023  , db_name:king_db
